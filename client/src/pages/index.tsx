ad80b4c6f36575e345a094b5e745ca15b8d7bdef
Raghav <raghavpillai101@gmail.com>
2023-05-02 01:48:20 -0500

    Added new topbar

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { Parallax } from "react-parallax";
-import { Text, Image, Button } from "@nextui-org/react";
-
+import { Navbar, Text, Image, Button } from "@nextui-org/react";
 
 type TypingProps = {
   texts: string[];
@@ -51,7 +50,13 @@ const Typing: React.FC<TypingProps> = ({ texts, delay = 1000 }) => {
   }, [currentText, currentTextIndex, delay, isTyping, texts]);
 
   return (
-    <Text h1 weight="bold" css={{ textAlign: "center" }} color="primary" size={60}>
+    <Text
+      h1
+      weight="bold"
+      css={{ textAlign: "center" }}
+      color="primary"
+      size={60}
+    >
       {currentText}
     </Text>
   );
@@ -60,11 +65,12 @@ const Typing: React.FC<TypingProps> = ({ texts, delay = 1000 }) => {
 interface SectionProps {
   title: string;
   children: React.ReactNode;
+  id?: string; // add id property
 }
 
-const Section = ({ title, children }: SectionProps) => {
+const Section = ({ title, children, id }: SectionProps) => {
   return (
-    <section className="min-h-screen">
+    <section id={id} className="min-h-screen">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold text-center mt-12 mb-6">{title}</h2>
         <div className="flex flex-wrap -mx-4">{children}</div>
@@ -77,8 +83,19 @@ const ParallaxSection = () => {
   return (
     <Parallax bgImage="/images/parallax-image2.gif" strength={500}>
       <div className="h-screen flex items-center justify-center relative">
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" style={{ overflowX: 'hidden' }}>
-          <Typing texts={["Caring made easier.", "Navigating Together.", "Helping You Thrive.", "Finding Your Way."]} delay={1500} />
+        <div
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          style={{ overflowX: "hidden" }}
+        >
+          <Typing
+            texts={[
+              "Caring made easier.",
+              "Navigating Together.",
+              "Helping You Thrive.",
+              "Finding Your Way.",
+            ]}
+            delay={1500}
+          />
         </div>
         <div className="absolute bottom-1/3 left-1/2 transform -translate-x-1/2 translate-y-1/2">
           <div className="flex items-center justify-between">
@@ -86,18 +103,14 @@ const ParallaxSection = () => {
               color="secondary"
               onClick={() =>
                 window.scrollTo({
-                  top: window.innerHeight-90,
+                  top: window.innerHeight - 90,
                   behavior: "smooth",
                 })
               }
             >
               Learn More
             </Button>
-            <Button 
-              color="secondary"
-              bordered
-              className="ml-4"
-            >
+            <Button color="secondary" bordered className="ml-4">
               Get Started
             </Button>
           </div>
@@ -108,60 +121,101 @@ const ParallaxSection = () => {
 };
 
 const Nav = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState("");
 
-  const handleToggle = () => {
-    setIsOpen(!isOpen);
+  useEffect(() => {
+    const handleScroll = () => {
+      const section1 = document.getElementById("section-1");
+      const section2 = document.getElementById("section-2");
+      if (section1 && window.scrollY < section1.offsetTop - 100) {
+        setActiveLink("");
+      } else if (section2 && window.scrollY >= section2.offsetTop - 100) {
+        setActiveLink("section-2");
+      } else {
+        setActiveLink("section-1");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScrollToSection1 = () => {
+    const section1 = document.getElementById("section-1");
+    if (section1) {
+      window.scrollTo({
+        top: section1.offsetTop - 90,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  const handleScrollToSection2 = () => {
+    const section2 = document.getElementById("section-2");
+    if (section2) {
+      window.scrollTo({
+        top: section2.offsetTop - 90,
+        behavior: "smooth",
+      });
+    }
   };
 
   return (
-      <nav
-        className="fixed top-0 left-0 w-full px-4 py-4 bg-green-900 bg-opacity-60 backdrop-filter backdrop-blur-lg z-50"
-      >
-        <div className="flex items-center justify-between">
-          <div className="flex items-center space-x-4">
-            <Image src="/logo.png" alt="Logo" width={40} height={40} />
-            <Text h2 weight="bold" 
-            css={{
-              textGradient: "45deg, $green900 -10%, $red800 50%",
-            }}
-            >Caring Guide</Text>
-          </div>
-          <div className="hidden md:flex items-center space-x-4">
-            <Button auto color="secondary">Login</Button>
-            <Button auto color="secondary">Sign Up</Button>
-          </div>
-          <div className="md:hidden cursor-pointer" onClick={handleToggle}>
-            <svg
-              className="h-6 w-6 fill-current text-black"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              {isOpen ? (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M19.41 16.41L18 15l-6 6-6-6L4.59 16.41L12 23.83l7.41-7.42z"
-                />
-              ) : (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M4 6h16v2H4V6zm0 5h16v2H4v-2zm0 5h16v2H4v-2z"
-                />
-              )}
-            </svg>
-          </div>
-        </div>
-        {isOpen && (
-          <div className="md:hidden absolute top-full left-0 w-full bg-white px-6 py-4 flex flex-col items-center space-y-4">
-            <Button auto >Login</Button>
-            <Button auto >Sign Up</Button>
-          </div>
-        )}
-      </nav>
-    )
-}
+    <Navbar
+      isBordered
+      variant="sticky"
+      maxWidth="fluid"
+      css={{
+        $$navbarBackgroundColor: "#FFFFFF80",
+        $$navbarBlurBackgroundColor: "#FFFFFF80",
+      }}
+    >
+      <Navbar.Brand>
+        <Image src="/logo.png" alt="Logo" width={40} height={40} />
+        <Text
+          h2
+          weight="bold"
+          css={{
+            textGradient: "45deg, $green900 -10%, $red800 50%",
+          }}
+        >
+          Caring Guide
+        </Text>
+      </Navbar.Brand>
+      <Navbar.Content hideIn="xs" variant="underline">
+        <Navbar.Link
+          activeColor="secondary"
+          isActive={activeLink === "section-1"}
+          color="primary"
+          onClick={handleScrollToSection1}
+        >
+          Section 1
+        </Navbar.Link>
+        <Navbar.Link
+          activeColor="secondary"
+          isActive={activeLink === "section-2"}
+          color="primary"
+          onClick={handleScrollToSection2}
+        >
+          Section 2
+        </Navbar.Link>
+      </Navbar.Content>
+      <Navbar.Content>
+        <Navbar.Link color="primary" href="#">
+          Login
+        </Navbar.Link>
+        <Navbar.Item>
+          <Button flat color="secondary" auto href="#">
+            Sign Up
+          </Button>
+        </Navbar.Item>
+      </Navbar.Content>
+    </Navbar>
+  );
+};
 
 type ContentSectionProps = {
   title: string;
@@ -195,7 +249,7 @@ const IndexPage = () => {
     <>
       <Nav />
       <ParallaxSection />
-      <Section title="Section 1">
+      <Section title="Section 1" id="section-1">
         <div className="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
           <div className="bg-white shadow-lg rounded-lg overflow-hidden">
             <Image
@@ -206,9 +260,16 @@ const IndexPage = () => {
               objectFit="cover"
             />
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Our Verification Process</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Our Verification Process
+              </h3>
               <p className="text-gray-700 text-base">
-              We take the safety and security of our users very seriously. That's why all of our guides go through a rigorous verification process before they are officially approved. This includes background checks, reference checks, and personal interviews. You can rest assured that you're in good hands with Caring Guide.
+                We take the safety and security of our users very seriously.
+                That's why all of our guides go through a rigorous verification
+                process before they are officially approved. This includes
+                background checks, reference checks, and personal interviews.
+                You can rest assured that you're in good hands with Caring
+                Guide.
               </p>
             </div>
           </div>
@@ -223,9 +284,17 @@ const IndexPage = () => {
               objectFit="cover"
             />
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Personalized Guidebook of Resources</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Personalized Guidebook of Resources
+              </h3>
               <p className="text-gray-700 text-base">
-              Our personalized guidebook covers a wide range of resource categories, including Physical, Emotional/Mental, Financial, Housing, Spiritual, End of Life, and General. This comprehensive approach ensures that caregivers have access to the support they need, when they need it. Plus, our mentorship program provides personalized guidance and support tailored to your unique situation.
+                Our personalized guidebook covers a wide range of resource
+                categories, including Physical, Emotional/Mental, Financial,
+                Housing, Spiritual, End of Life, and General. This comprehensive
+                approach ensures that caregivers have access to the support they
+                need, when they need it. Plus, our mentorship program provides
+                personalized guidance and support tailored to your unique
+                situation.
               </p>
             </div>
           </div>
@@ -240,22 +309,30 @@ const IndexPage = () => {
               objectFit="cover"
             />
             <div className="p-4">
-              <h3 className="text-xl font-bold mb-2">Connect with a Personal Mentor</h3>
+              <h3 className="text-xl font-bold mb-2">
+                Connect with a Personal Mentor
+              </h3>
               <p className="text-gray-700 text-base">
-              At Caring Guide, we understand that caregiving can be overwhelming and stressful. That's why we connect caregivers with a personal mentor who provides emotional and practical support throughout their caregiving journey.
+                At Caring Guide, we understand that caregiving can be
+                overwhelming and stressful. That's why we connect caregivers
+                with a personal mentor who provides emotional and practical
+                support throughout their caregiving journey.
               </p>
             </div>
           </div>
         </div>
       </Section>
-      <Section title="Section 2">
-        
+      <Section title="Section 2" id="section-2">
         <div className="flex items-center justify-between">
           <Text>
-          Whether you're an early-stage caregiver or an experienced veteran, we've got you covered. Our personalized guidebook and mentorship program are designed to meet the needs of caregivers at every stage of their journey. Plus, our user profiles allow you to connect with other caregivers who are at a similar stage, so you can learn from each other and grow together.
+            Whether you're an early-stage caregiver or an experienced veteran,
+            we've got you covered. Our personalized guidebook and mentorship
+            program are designed to meet the needs of caregivers at every stage
+            of their journey. Plus, our user profiles allow you to connect with
+            other caregivers who are at a similar stage, so you can learn from
+            each other and grow together.
           </Text>
-          </div>  
-        
+        </div>
       </Section>
     </>
   );
