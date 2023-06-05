0ebd18a492e9f9c09942dd097b77e659aea6dbe5
IbrahimKhanGH <iboo11khan@gmail.com>
2023-06-05 13:10:27 -0500

    homepage template

---
+++ client/src/pages/api/articlesData.js
@@ -0,0 +1,105 @@
+const articles = [
+    {
+        title: "The Cat in the Hat",
+        author: "Dr. Seuss",
+        image: "/dashimgs/placeholder.png",
+      },
+      {
+        title: "Green Eggs and Ham",
+        author: "Dr. Seuss",
+        image: "/dashimgs/placeholder.png",
+      },
+      {
+        title: "Oh, the Places You'll Go!",
+        author: "Dr. Seuss",
+        image: "/dashimgs/placeholder.png",
+      },
+      {
+        title: "Fox in Socks",
+        author: "Dr. Seuss",
+        image: "/dashimgs/placeholder.png",
+      },
+      {
+        title: "One Fish Two Fish Red Fish Blue Fish",
+        author: "Dr. Seuss",
+        image: "/dashimgs/placeholder.png",
+      },
+    {
+      title: "The Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "To Kill a Mockingbird",
+      author: "Harper Lee",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "1984",
+      author: "George Orwell",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "Pride and Prejudice",
+      author: "Jane Austen",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Catcher in the Rye",
+      author: "J.D. Salinger",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "Moby-Dick",
+      author: "Herman Melville",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "Jane Eyre",
+      author: "Charlotte Bronte",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Lord of the Rings",
+      author: "J.R.R. Tolkien",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "Harry Potter and the Sorcerer's Stone",
+      author: "J.K. Rowling",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Chronicles of Narnia",
+      author: "C.S. Lewis",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Odyssey",
+      author: "Homer",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Alchemist",
+      author: "Paulo Coelho",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Hobbit",
+      author: "J.R.R. Tolkien",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "Frankenstein",
+      author: "Mary Shelley",
+      image: "/dashimgs/placeholder.png",
+    },
+    {
+      title: "The Picture of Dorian Gray",
+      author: "Oscar Wilde",
+      image: "/dashimgs/placeholder.png",
+    },
+  ];
+
+  
+  module.exports = articles;
--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,115 +1,190 @@
 import { Text, Image, Col, Card } from "@nextui-org/react";
 
 import Section from "./components/section";
-import ParallaxSection  from "./components/parallaxsection";
+import ParallaxSection from "./components/parallaxsection";
 import Footer from "./components/footer";
 import Nav from "./components/nav";
+import pfp from "./tempimages/profile-picture.jpg";
+import articles from "./api/articlesData";
+import journal from "./api/journalData";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import AddIcon from "@mui/icons-material/Add";
 
 const HomePage = () => {
-    return (
-    
-      <>
-<Section title="Mentor Display/Journal/Article">
-        <div className="flex">
-          <div className="w-1/2">
-            <div
-              className="bg-white shadow-lg rounded-lg text-center mx-2 flex"
-              style={{
-                justifyContent: "flex-start",
-                alignSelf: "flex-start",
-              }}
-            >
-              <div className="w-1/4">
-                <img
-                  src="https://via.placeholder.com/150"
-                  alt="Mentor Image 1"
-                  className="w-full h-auto object-cover rounded-lg"
-                />
-              </div>
-              <div className="w-3/4 px-4 py-2">
-                <h2 className="text-xl font-bold">Mentor Name 1</h2>
-                <p className="text-gray-600">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Suspendisse malesuada ante eget dui sodales, auctor
-                  ullamcorper leo lacinia. Phasellus eu mi neque. Donec
-                  sollicitudin lacus eget nunc egestas, quis venenatis odio
-                  vestibulum
-                </p>
-              </div>
+  return (
+    <>
+      {/*Full Page Div*/}
+      <div className="w-max-11/12 flex m-8" style={{ height: "755px" }}>
+        {/* Account/Mentor Div */}
+        <div className="w-[25%] h-full">
+          {/* Account Rectangle */}
+          <div className="border border-gray-300 border-2 rounded-xl flex items-center m-1 p-1 mb-5">
+            {/* User Profile Picture */}
+            <div className="w-1/3 flex items-center justify-center m-1">
+              <div
+                className="rounded-full h-20 w-20 bg-cover bg-center"
+                style={{
+                  backgroundImage: `url('/dashimgs/placeholder.png')`,
+                }}
+              ></div>
             </div>
-            <div
-              className="bg-white shadow-lg rounded-lg text-center mx-2 flex flex-col"
-              style={{ width: "97.5%", marginTop: "2rem", height: "300px" }}
-            >
-              <h2 className="text-xl font-bold mb-2 py-2">Favorited Articles</h2>
-              <div className="flex justify-center py-7">
-                <div className="mx-2">
-                  <img
-                    src="https://via.placeholder.com/100"
-                    alt="Article 1"
-                    className="w-full h-auto object-cover rounded-lg"
-                  />
-                  <p className="mt-1 font-bold">Article Title 1</p>
-                </div>
-                <div className="mx-2">
-                  <img
-                    src="https://via.placeholder.com/100"
-                    alt="Article 2"
-                    className="w-full h-auto object-cover rounded-lg"
-                  />
-                  <p className="mt-1 font-bold">Article Title 2</p>
-                </div>
-                <div className="mx-2">
-                  <img
-                    src="https://via.placeholder.com/100"
-                    alt="Article 3"
-                    className="w-full h-auto object-cover rounded-lg"
-                  />
-                  <p className="mt-1 font-bold">Article Title 3</p>
-                </div>
-                <div className="mx-2">
-                  <img
-                    src="https://via.placeholder.com/100"
-                    alt="Article 4"
-                    className="w-full h-auto object-cover rounded-lg"
-                  />
-                  <p className="mt-1 font-bold">Article Title 4</p>
-                </div>
-                <div className="mx-2">
-                  <img
-                    src="https://via.placeholder.com/100"
-                    alt="Article 5"
-                    className="w-full h-auto object-cover rounded-lg"
-                  />
-                  <p className="mt-1 font-bold">Article Title 5</p>
-                </div>
-              </div>
+            {/* Name/View Profile */}
+            <div className="w-2/3 flex flex-col justify-center ">
+              <h2 className="text-2xl ">MENTEE NAME</h2>
+              <a href="/profile" className="text-gray-500 mt-[-0.75rem]">
+                view my profile
+              </a>
             </div>
           </div>
-          <div className="w-1/2">
-            <div
-              className="bg-white shadow-lg rounded-lg text-center mx-2 flex "
-              style={{ height: "500px" }}
-            >
-              <div className="flex-col justify-center w-full h-full py-2">
-                <h2 className="text-xl font-bold">Journal</h2>
+
+          {/* Mentor Profile */}
+          <div className="h-5/6 border border-gray-300 border-2 rounded-xl m-1 p-4 pr-2 flex flex-col">
+            <div className="text-2xl font-bold mb-4">My Mentor</div>
+            {/* Mentor Profile Picture and Name/Detail */}
+            <div className="flex items-center mt-[-0.5rem]">
+              {/* Mentor Profile Picture */}
+              <div className="w-1/3 flex m-1">
                 <div
-                  className="w-full h-full flex flex-col px-4"
-                  style={{ height: "calc(97% - 2rem)" }}
-                >
-                  <textarea
-                    className="w-full h-full border border-gray-400 p-2"
-                    style={{ boxSizing: "border-box", resize: "none" }}
-                    placeholder="Enter Text Here"
-                  ></textarea>
+                  className="rounded-full h-20 w-20 bg-cover bg-center"
+                  style={{
+                    backgroundImage: `url('/dashimgs/placeholder.png')`,
+                  }}
+                ></div>
+              </div>
+              {/* Mentor Name/Detail */}
+              <div className="w-2/3 flex flex-col justify-center">
+                <div>
+                  <h1 className="text-xl inline">MENTOR NAME</h1>
+                  <p className="text-gray-400 text-xs inline ml-2">pronoun</p>
+                </div>
+                <div className="flex text-sm">
+                  <p className="text-gray-500 text-xs mr-2">AGE</p>
+                  <p className="text-xs mr-2">&middot;</p>
+                  <p className="text-gray-500 text-xs">DISEASE</p>
                 </div>
               </div>
+            </div>
+            {/* Mentor About Section */}
+            <div className="mt-4">
+              <h1 className="text-lg">About</h1>
+              <p className="text-xs text-gray-500 mt-[-0.25rem] mr-2">
+                Lorem ipsum dolor sit amet, ea mundi altera vim, eu mel munere
+                legendos, vim ne eligendi repudiare consequat. Fugit molestie
+                expetendis id has, vix eu malorum impedit honestatis. Te sed
+                augue ubique populo, sit diceret numquam eu. Iuvaret dignissim
+                sed cu, eu altera periculis eos. Duo ut aperiri epicurei, vim ne
+                hinc offendit molestiae.
+              </p>
+            </div>
+            {/* Mentor Contact Section */}
+            <div className="mt-4">
+              <h1 className="text-lg">Contact Information</h1>
+            </div>
+            {/* Contact Rectangle */}
+            <div className="bg-gray-200 mt-[-.25rem] mr-2 p-2 rounded-xl">
+              <div className="flex flex-col">
+                <div className="flex items-center">
+                  <span
+                    style={{ fontSize: "0.5rem" }}
+                    className="text-gray-600"
+                  >
+                    PHONE NUMBER
+                  </span>
+                </div>
+                <div className="flex items-center text-sm mt-[-0.1rem]">
+                  <span>INSERT-PHONE-NUMBER</span>
+                </div>
+                <div className="flex items-center mt-1">
+                  <span
+                    style={{ fontSize: "0.5rem" }}
+                    className="text-gray-600"
+                  >
+                    EMAIL
+                  </span>
+                </div>
+                <div className="flex items-center text-sm mt-[-0.1rem]">
+                  <span>INSERT@EMAIL.COM</span>
+                </div>
+              </div>
+            </div>
+            {/* Experience Section */}
+            <div className="mt-4">
+              <h1 className="text-lg">Caregiving Experience</h1>
+            </div>
+            <div className="bg-gray-200 mt-[-.25rem] mr-2 p-2 rounded-xl text-sm">
+              INSERT YEARS
+            </div>
+            {/* Open Profile Section */}
+            <div className="mt-6 mr-2">
+              <button className="w-full text-gray-600 border border-gray-300 border-2 rounded-lg py-2 px-4">
+                Open Profile
+              </button>
             </div>
           </div>
         </div>
-      </Section>
-      </>
-    );
+
+        {/* Article List */}
+        <div className="w-[50%] h-full w-1/4 border border-gray-300 border-2 rounded-xl m-2 mr-6 ml-6 p-4 pr-0">
+          <div className="text-2xl font-bold mb-4">Favorite Articles</div>
+          <div
+            className="flex flex-wrap overflow-y-auto"
+            style={{ maxHeight: "687px" }}
+          >
+            {articles.map((article, index) => (
+              <div className="w-48 h-48 rounded-xl mr-5 mb-5" key={index}>
+                {/* Article Image */}
+                <div
+                  className="h-36 w-48 bg-cover bg-center rounded-t-xl"
+                  style={{
+                    backgroundImage: `url(${article.image})`,
+                  }}
+                ></div>
+                <div className="bg-gray-200 p-2 rounded-b-xl w-48 relative">
+                  {/* Article Title */}
+                  <h3 className="text-xs font-bold mb-1 truncate">
+                    {article.title}
+                  </h3>
+                  {/* Article Author */}
+                  <p className="text-gray-600 text-xs">{article.author}</p>
+                  <div className="absolute bottom-2 right-2">
+                    <FavoriteIcon style={{ fontSize: "1rem" }} />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Journal List */}
+        <div className="w-[24%] h-full border border-gray-300 border-2 rounded-xl m-2 p-3">
+          <div className="flex items-center justify-between mb-4">
+            <div className="text-2xl font-bold">Journal</div>
+            <div className="text-gray-500">
+              <AddIcon />
+            </div>
+          </div>
+          <div
+            className="grid grid-cols-1 gap-4 overflow-y-auto"
+            style={{ maxHeight: "690px" }}
+          >
+            {journal.map((item, index) => (
+              <div
+                key={index}
+                className="bg-gray-200 p-2 rounded-xl"
+                style={{ minHeight: "100px" }} // Adjust the height as needed
+              >
+                <div className="flex">
+                  <h3 className="text-sm font-bold mr-2">{item.title}</h3>
+                  <div className="text-xs text-gray-500">{item.date}</div>
+                </div>
+                <p className="text-xs mt-1">{item.description}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
