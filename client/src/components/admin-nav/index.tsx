ede0bdf421b7bdfd09e941a19b760ed4f1998404
Ali Abbas <maliabbas2001@gmail.com>
2023-08-25 02:42:21 +0500

    welcome page made

--- client/src/components/admin-nav/index.tsx
+++ client/src/components/admin-nav/index.tsx
@@ -1,14 +1,14 @@
-import { useRouter } from "next/router";
-import { PropsWithChildren, useEffect } from "react";
-import { Role } from "@prisma/client";
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
+import { useApp } from "@/context/app";
+import { cn } from "@/lib/utils";
+import { Text } from "@nextui-org/react";
+import { Role } from "@prisma/client";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { PowerIcon, Tag, User2Icon } from "lucide-react";
 import Image from "next/image";
-import { Text } from "@nextui-org/react";
-import { useApp } from "@/context/app";
+import { useRouter } from "next/router";
+import { PropsWithChildren, useEffect } from "react";
 import { useLocalStorage } from "usehooks-ts";
-import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 export default function AdminDashboard({ children }: PropsWithChildren) {
   const router = useRouter();
@@ -26,7 +26,7 @@ export default function AdminDashboard({ children }: PropsWithChildren) {
     }
 
     if (!profile) {
-      router.push("/onboarding");
+      router.push("/welcome");
       return;
     }
 
@@ -43,8 +43,8 @@ export default function AdminDashboard({ children }: PropsWithChildren) {
           "hidden w-full max-w-[18rem] border-r border-zinc-200 pb-12 dark:border-zinc-900 md:block"
         )}
       >
-        <div className="space-y-4 py-4">
-          <div className="mr-1 flex justify-center px-3">
+        <div className="py-4 space-y-4">
+          <div className="flex justify-center px-3 mr-1">
             <Image src="/logo.png" alt="Logo" width={40} height={40} />
             <Text
               h4
@@ -58,45 +58,45 @@ export default function AdminDashboard({ children }: PropsWithChildren) {
           </div>
 
           <div className="px-3 py-2">
-            <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight">
+            <h2 className="px-4 mb-2 text-lg font-semibold tracking-tight">
               Dashboard
             </h2>
             <div className="space-y-1">
               <Button
-                className="w-full justify-start"
+                className="justify-start w-full"
                 variant={
                   route.startsWith("/admin/accounts") ? "secondary" : "ghost"
                 }
                 onClick={() => router.push("/admin/accounts")}
               >
-                <User2Icon className="mr-2 h-4 w-4" />
+                <User2Icon className="w-4 h-4 mr-2" />
                 Accounts
               </Button>
               <Button
-                className="w-full justify-start"
+                className="justify-start w-full"
                 variant={
                   route.startsWith("/admin/resources") ? "secondary" : "ghost"
                 }
                 onClick={() => router.push("/admin/resources")}
               >
-                <Tag className="mr-2 h-4 w-4" />
+                <Tag className="w-4 h-4 mr-2" />
                 Resources
               </Button>
               <Button
-                className="w-full justify-start"
+                className="justify-start w-full"
                 variant={
                   route.startsWith("/admin/resources") ? "secondary" : "ghost"
                 }
                 onClick={() => supabase.auth.signOut()}
               >
-                <PowerIcon className="mr-2 h-4 w-4" />
+                <PowerIcon className="w-4 h-4 mr-2" />
                 Logout
               </Button>
             </div>
           </div>
         </div>
       </nav>
-      <div className="h-screen w-full overflow-hidden">{children}</div>
+      <div className="w-full h-screen overflow-hidden">{children}</div>
     </div>
   );
 }
