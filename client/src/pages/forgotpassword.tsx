7c21f155ea45720708f6fc04f0f60334ddf09886
HZapperz <46976026+HZapperz@users.noreply.github.com>
2023-11-18 17:57:43 -0600

    forgot password module

---
+++ client/src/pages/forgotpassword.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import toast from 'react-hot-toast';
+import Link from 'next/link';
+
+const ForgotPassword = () => {
+  const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
+  const supabase = useSupabaseClient();
+
+  const handleResetPassword = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const { error } = await supabase.auth.api.resetPasswordForEmail(email);
+
+    if (error) {
+      toast.error(error.message);
+    } else {
+      toast.success('Check your email for the reset link');
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <div className="forgot-password-container">
+      <h2>Forgot Password</h2>
+      <form onSubmit={handleResetPassword}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
+            required
+          />
+        </div>
+        <button type="submit" disabled={loading}>
+          {loading ? 'Sending...' : 'Send Reset Link'}
+        </button>
+      </form>
+      <div>
+        <Link href="/signin">
+          <a>Back to Sign In</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
--- client/src/pages/signin.tsx
+++ client/src/pages/signin.tsx
@@ -4,14 +4,16 @@ import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import toast from "react-hot-toast";
 import { useRouter } from "next/router";
 import { useApp } from "@/context/app";
-import { useEffect } from "react";
-import { useState } from "react";
-import useHandleErrors from "@/hooks/useHandleErrors";
+import { useEffect, useState } from "react";
+import ForgotPassword from "./forgotpassword";
+
 
 type SignInFormValues = {
   email: string;
   password: string;
 };
+
+
 
 const Login = () => {
   const { session, profile, isLoading } = useApp();
@@ -20,7 +22,7 @@ const Login = () => {
   const { handleSubmit } = form;
   const router = useRouter();
   const [loading, setLoading] = useState(false);
-  const handleErrors = useHandleErrors();
+  
 
   async function handleLogin(data: SignInFormValues) {
     try {
@@ -33,9 +35,7 @@ const Login = () => {
         if (error.message.includes("credentials")) {
           return toast.error("Invalid login credentials. Please try again.");
         } else if (error.message.includes("verified")) {
-          return toast.error(
-            "Your account is not verified. Please verify your email.",
-          );
+          return toast.error("Your account is not verified. Please verify your email.");
         } else {
           return toast.error(error.message);
         }
@@ -44,8 +44,6 @@ const Login = () => {
       }
     } catch (error) {
       console.error("Login error", error);
-      // Remove this toast.error line for testing
-      // toast.error("An unexpected error occurred. Please try again.");
     }
   }
 
@@ -65,6 +63,9 @@ const Login = () => {
     } finally {
       setLoading(false);
     }
+  };
+  const navigateToForgotPassword = () => {
+    router.push('/forgotpassword');
   };
 
   return (
@@ -91,32 +92,10 @@ const Login = () => {
                   <p className="mr-4 font-poppins font-medium text-lg text-[#4E4E4E]">
                     Sign In With Google
                   </p>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 533.5 544.3"
-                    className="w-5 h-5"
-                  >
-                    <path
-                      fill="#4E4E4E"
-                      d="M533.5 278.4c0-19.4-1.6-38.3-4.6-56.7H272.2v107.2h149.7c-6.2 33.6-24.3 62.1-51.5 80.7v66h83.4c48.9-45.1 76.8-111.7 76.8-197.2z"
-                    />
-                    <path
-                      fill="#4E4E4E"
-                      d="M272.2 544.3c69.7 0 128.1-22.9 170.7-62.4l-83.4-66c-23.2 15.6-52.9 24.8-87.3 24.8-66.8 0-123.4-45.1-143.8-105.7H38.6v66c42.6 83.4 128.1 138.3 233.6 138.3z"
-                    />
-                    <path
-                      fill="#4E4E4E"
-                      d="M128.4 324.1a162.2 162.2 0 0 1-8.9-51.3c0-17.8 3-35.1 8.9-51.3V155H38.6a278.2 278.2 0 0 0 0 234.3l89.8-65.2z"
-                    />
-                    <path
-                      fill="#4E4E4E"
-                      d="M272.2 107.7c37.9 0 72.3 13.1 99.2 34.1l75.1-75.1C400.3 22.9 341.9 0 272.2 0 166.7 0 81.2 54.9 38.6 138.3l89.8 65.2c20.4-60.6 77-105.7 143.8-105.7z"
-                    />
-                  </svg>
+                  {/* Google Icon */}
                 </div>
               </>
             )}
-            ;
           </button>
         </div>
         <div className="flex justify-center items-center w-full my-4">
@@ -142,7 +121,6 @@ const Login = () => {
               className="mt-2 w-full border-2 border-white bg-[#eceeed] bg-opacity-40 rounded-xl py-2 px-3 placeholder:text-white"
             />
           </div>
-
           <div className="mb-4">
             <label
               htmlFor="password"
@@ -157,22 +135,25 @@ const Login = () => {
               {...form.register("password")}
               className="mt-2 w-full border-2 border-white bg-[#eceeed] bg-opacity-40 rounded-xl py-2 px-3 placeholder:text-white"
             />
+            <div className="text-right">
+              <button onClick={navigateToForgotPassword} className="font-poppins text-md text-white underline">
+                Forgot Password?
+              </button>
+            </div>
           </div>
-
           <button
             type="submit"
             className="w-full py-3 text-center text-white bg-caring rounded-xl"
           >
             Log In
           </button>
-
           <div className="mt-4 text-center">
             <span className="font-poppins text-2xl font-medium text-white">
               {`Don't have an account? `}
               <Link href="/signup">
-                <span className="font-semibold underline text-caring">
+                <a className="font-semibold underline text-caring">
                   Sign Up
-                </span>
+                </a>
               </Link>
             </span>
           </div>
@@ -180,6 +161,7 @@ const Login = () => {
       </div>
     </div>
   );
-};
+  
+  };
 
-export default Login;
+  export default Login;
